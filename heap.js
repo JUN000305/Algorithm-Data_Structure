48aa1445f1a68f430c747b28dfffb9b33745d56a
Jun <99010586+JUN000305@users.noreply.github.com>
2022-05-18 10:34:12 +0900

    힙 자료구조, 삽입 구현

---
+++ heap.js
@@ -0,0 +1,31 @@
+'use strict';
+
+class Heap {
+  constructor() {
+    this.heap = [null];
+  }
+
+  getMin() {
+    return this.heap[1] ? this.heap[1] : null;
+  }
+
+  swap(a, b) {
+    [this.heap[a], this.heap[b]] = [this.heap[b], this.heap[a]];
+  }
+
+  heappush(value) {
+    this.heap.push(value);
+    let curIdx = this.heap.length - 1;
+    let parIdx = Math.floor(curIdx / 2);
+
+    while (curIdx > 1 && this.heap[parIdx] > this.heap[curIdx]) {
+      this.swap(parIdx, curIdx);
+      curIdx = parIdx;
+      parIdx = Math.floor(curIdx / 2);
+    }
+  }
+}
+
+let newHeap = new Heap();
+
+console.log(1.5 >> 0);
