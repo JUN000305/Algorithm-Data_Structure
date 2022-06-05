0a80b85bc93fc9445bfc098855a37909392daa9b
Jun <jsj17077@gmail.com>
2022-06-05 20:27:26 +0900

    힙 구현 완료

--- heap.js
+++ heap.js
@@ -24,8 +24,49 @@ class Heap {
       parIdx = Math.floor(curIdx / 2);
     }
   }
+
+  heappop() {
+    const min = this.heap[1];
+    if (this.heap.length <= 2) {
+      this.heap = [null];
+    } else this.heap[1] = this.heap.pop(); // <- 배열의 마지막 값
+
+    let curIdx = 1;
+    let leftIdx = curIdx * 2;
+    let rightIdx = curIdx * 2 + 1;
+
+    if (!this.heap[leftIdx]) {
+      return min;
+    }
+
+    if (!this.heap[rightIdx]) {
+      if (this.heap[leftIdx] < this.heap[curIdx]) {
+        this.swap(leftIdx, curIdx);
+      }
+      return min;
+    }
+
+    while (
+      this.heap[leftIdx] < this.heap[curIdx] ||
+      this.heap[rightIdx] < this.heap[curIdx]
+    ) {
+      const minIdx =
+        this.heap[leftIdx] > this.heap[rightIdx] ? rightIdx : leftIdx;
+      this.swap(minIdx, curIdx);
+      curIdx = minIdx;
+      leftIdx = curIdx * 2;
+      rightIdx = curIdx * 2 + 1;
+    }
+    return min;
+  }
 }
 
 let newHeap = new Heap();
 
-console.log(1.5 >> 0);
+newHeap.heappush(3);
+newHeap.heappush(2);
+newHeap.heappush(1);
+newHeap.heappush(5);
+newHeap.heappush(0);
+
+console.log(newHeap);
